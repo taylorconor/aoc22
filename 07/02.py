c7c7dd6488251c02a4c0f0403877b6b512d39b89
Conor Taylor <conor@taylor.sh>
2022-12-07 01:24:30 -0500

    Day 7

---
+++ 07/02.py
@@ -0,0 +1,54 @@
+with open('07/input.txt') as f:
+    lines = f.read().splitlines()[1:]
+
+class File:
+    def __init__(self, size, name):
+        self.size = size
+        self.name = name
+    name = ""
+    size = 0
+
+class Dir:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.dirs = {}
+        self.files = {}
+        if parent is not None:
+            self.full_id = parent.full_id + "/" + name
+    def file_size(self):
+        return sum([f.size for f in self.files.values()])
+    full_id = ""
+
+fs = Dir("/", None)
+root = fs
+for line in lines:
+    if line == "$ ls":
+        continue
+    if line == "$ cd ..":
+        if fs.parent is not None:
+            fs = fs.parent
+        continue
+    if line.startswith("$ cd "):
+        name = line.split()[-1]
+        if name not in fs.dirs:
+            continue
+        fs = fs.dirs[name]
+        continue
+    if line.startswith("dir"):
+        name = line.split()[-1]
+        fs.dirs[name] = Dir(name, fs)
+        continue
+    size, name = line.split()
+    fs.files[name] = File(int(size), name)
+
+target = size = 9199225
+def add_sizes(root):
+    global size
+    result = root.file_size() + sum([add_sizes(f) for f in list(root.dirs.values())])
+    if result > target and result - target < size:
+        size = result
+    return result
+
+total = add_sizes(root)
+print("size = " + str(size))
